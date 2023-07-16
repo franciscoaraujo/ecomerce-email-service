5cc3fc6a36d84bea74cdcf1312b0cbb1544812a0
Francisco Araujo <francisco.araujo@flexpagamentos.com.br>
2023-07-16 18:38:47 -0300

    first commit

---
+++ ms-email-service/src/main/java/br/com/fiaptcc/msemailservice/MsEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiaptcc.msemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailServiceApplication.class, args);
+	}
+
+}
---
+++ ms-email-service/src/test/java/br/com/fiaptcc/msemailservice/MsEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiaptcc.msemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
